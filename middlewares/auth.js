fd05c3f13d8a2fb92b944cbbaf244815c72ff0e1
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-03-08 14:27:45 +0530

    feat(auth): added update password API for logged-in users

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -14,7 +14,33 @@ const UserAuth = async (req, res, next) => {
     const user = await jwt.verify(token, "WebSecretToken@987");
     const { _id } = user;
 
-    const foundUser = await User.findById(_id).select("firstName lastName"); //console.log(foundUser);
+    const foundUser = await User.findById(_id).select(
+      "firstName lastName password"
+    ); //console.log(foundUser);
+    if (!foundUser) {
+      res.status(404).send("User Not Found ☹");
+    }
+
+    req.user = foundUser;
+    next();
+  } catch (err) {
+    res.status(404).send("Error Occrured :" + err);
+  }
+};
+
+const PasswordAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      res.status(401).send("Please Log-In Again");
+    }
+
+    const user = await jwt.verify(token, "WebSecretToken@987");
+    const { _id } = user;
+
+    const foundUser = await User.findById(_id).select(
+      "firstName lastName password"
+    ); //console.log(foundUser);
     if (!foundUser) {
       res.status(404).send("User Not Found ☹");
     }
@@ -28,4 +54,5 @@ const UserAuth = async (req, res, next) => {
 
 module.exports = {
   UserAuth,
+  PasswordAuth,
 };
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const userRouter = express.Router();
-const { UserAuth } = require("../middlewares/auth");
+const { UserAuth, PasswordAuth } = require("../middlewares/auth");
 const { validateProfileData } = require("../utils/validation");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
@@ -26,6 +27,32 @@ userRouter.patch("/user/edit", UserAuth, async (req, res) => {
     res.status(500).send("Something Went Wrong !");
   }
 });
+
+userRouter.patch("/user/updatepassword", PasswordAuth, async (req, res) => {
+  try {
+    const { oldPassword, newPassword } = req.body;
+
+    loggedInUser = req.user;
+    if (!oldPassword || !newPassword) {
+      res.status(404).send("Both old and New Passwords are Required");
+    }
+
+    const isMatch = await bcrypt.compare(oldPassword, loggedInUser.password);
+    if (!isMatch) {
+      res.status(400).send("Old Password is Incorrect");
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    loggedInUser.password = hashedPassword;
+    await loggedInUser.save();
+
+    res.status(200).send("Password Updated SuccessFully");
+  } catch (err) {
+    res.status(500).send("Something Went Wrong !" + err);
+  }
+});
+
 module.exports = {
   userRouter,
 };
